16de32214f272a63eb0c4c89234965557d209e49
Daniel Molina <danielmolina9202@gmail.com>
2024-07-07 19:32:01 -0400

    realizacion del renderizado de las primeras tareas definidas

---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+const listContainer= document.querySelector("#listContainer")
+const addTaskButton= document.querySelector("#addTask")
+const taskName= document.querySelector("#taskName")
+
+const toDoList= [
+    {
+        id:1,
+        task: "Tarea 1",
+        done: false
+    },
+    {
+        id:2,
+        task: "Tarea 2",
+        done: false
+    },
+    {
+        id:1,
+        task: "Tarea 3",
+        done: true
+    },
+]
+
+const renderTask = ()=> {
+    let html = ""
+    toDoList.forEach((task)=> {
+        html += /*html*/`
+            <li id="${task.id}">
+                <span>${task.task}</span>
+                <span>
+                    <input type="checkbox" class="toggle-done" ${task.done ? 'checked' : ''}>
+                   <span>&#x274c</span>
+                </span>
+            </li>
+        `
+    })
+    listContainer.innerHTML = html
+}
+
+renderTask()
+
+addTaskButton.addEventListener("click", ()=>{
+    const task = {
+        id: Date.now(),
+        task: taskName.value,
+        done: false
+    }
+    toDoList.push(task)
+    renderTask()
+    taskName.value = "";
+})
+
+
+
+
+
