31cf196d78bc607f061afe251886a318993029fc
Daniel Molina <danielmolina9202@gmail.com>
2024-07-08 13:27:29 -0400

    eliminar tarea en proceso

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,34 @@
-const listContainer= document.querySelector("#listContainer")
-const addTaskButton= document.querySelector("#addTask")
-const taskName= document.querySelector("#taskName")
+const listContainer = document.querySelector("#listContainer")
+const addTaskButton = document.querySelector("#addTask")
+const taskName = document.querySelector("#taskName")
 
 
-const toDoList= [
+const toDoList = [
     {
-        id:1,
+        id: 1,
         task: "Tarea 1",
         done: false
     },
     {
-        id:2,
+        id: 2,
         task: "Tarea 2",
         done: false
     },
     {
-        id:1,
+        id: 3,
         task: "Tarea 3",
         done: false
     },
 ]
 
-const renderTask = ()=> {
+const renderTask = () => {
     let html = ""
-    toDoList.forEach((task)=> {
+    toDoList.forEach((task) => {
         html += /*html*/`
             <li id="${task.id}">
                 <span>${task.task}</span>
-                <span>
                     <input type="checkbox" class="toggle-done" ${task.done ? 'checked' : ''}>
                    <span id="delete">&#x274c</span>
-                </span>
             </li>
         `
     })
@@ -40,22 +38,30 @@ const renderTask = ()=> {
 renderTask()
 
 const addTask = () => {
-        const task = {
-            id: Date.now(),
-            task: taskName.value,
-            done: false
-        }
-        toDoList.push(task)
+    const task = {
+        id: Date.now(),
+        task: taskName.value,
+        done: false
+    }
+    toDoList.push(task)
+    renderTask()
+    taskName.value = ""
+}
+
+addTaskButton.addEventListener("click", addTask)
+
+
+const deletetasks = document.querySelectorAll("#delete")
+
+deletetasks.forEach(deletetask => {    
+    deletetask.addEventListener("click", () => {
+        let indice= deletetask.parentNode.id
+        console.log(indice)
+        toDoList.splice(indice-1,1)
+        console.log(toDoList)
         renderTask()
-        taskName.value = "";
-}
-
-addTaskButton.addEventListener("click", () => addTask)
-
-const deleteTask = ()=> {
-    const deletetask= document.querySelector("#delete")
-    
-}
+    });
+});
 
 
 
